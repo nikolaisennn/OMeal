deccb4f945e1f71037f3fa621396ca76b25695d6
Nikolay Dimitrov <nikolaydimitrovvv@gmail.com>
2024-08-10 13:59:33 +0300

    Login and register html pages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const morgan = require('morgan');
 
 const productsRouter = require('./routes/products')
 
@@ -12,6 +13,8 @@ app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
 app.use(express.urlencoded({ extended: false }))
 // parse json
 app.use(express.json())
+// Morgan middleware in 'dev' mode
+app.use(morgan('tiny'));
 
 app.use('/api/products', productsRouter)
 
